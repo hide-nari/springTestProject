71ee1f77cd9259d56a9614c1cd546056a36ae9fb
hide-nari <hide3@mild.ocn.ne.jp>
2026-04-26 07:46:15 +0900

    Add post route

--- src/main/java/com/hidenari/springboottutorial/SpringBootTutorialApplication.java
+++ src/main/java/com/hidenari/springboottutorial/SpringBootTutorialApplication.java
@@ -3,6 +3,7 @@ package com.hidenari.springboottutorial;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,4 +20,8 @@ public class SpringBootTutorialApplication {
         return String.format("Hello %s!", name);
     }
 
+    @PostMapping("/post")
+    public String postMethod(@RequestParam String name) {
+        return "Post:" + name;
+    }
 }
